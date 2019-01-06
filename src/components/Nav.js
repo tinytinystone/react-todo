b5edbd3b634696971d88f1e7b533906cac4a34b5
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-01-07 01:34:27 +0900

    스타일링 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,25 +27,31 @@ export default class Nav extends Component {
   render() {
     return (
       <nav className={s.nav}>
-        <ul>
+        <ul className={s.topFilters}>
           <li>관리함</li>
           <li>오늘</li>
           <li>다음 7일</li>
         </ul>
-        <div>
-          <span>프로젝트</span>
-          <button onClick={this.addProject}>+</button>
-          {this.state.showProjectForm && <ProjectForm />}
-          <ul>
-            {this.state.projects.map(p => (
-              <Link to={'/projects/' + p.id}>
-                <li key={p.id}>{p.title}</li>
-              </Link>
-            ))}
-          </ul>
+        <div className={s.panels}>
+          <div className={s.project}>
+            <span className={s.panelSummary}>프로젝트</span>
+            {this.state.showProjectForm && <ProjectForm />}
+            <ul>
+              {this.state.projects.map(p => (
+                <Link to={'/projects/' + p.id} key={p.id}>
+                  <li key={p.id}>{p.title}</li>
+                </Link>
+              ))}
+            </ul>
+            <button onClick={this.addProject}>+</button>
+          </div>
         </div>
-        <div>라벨</div>
-        <div>필터</div>
+        <div className={s.panels}>
+          <span className={s.panelSummary}>라벨</span>
+        </div>
+        <div className={s.panels}>
+          <span className={s.panelSummary}>필터</span>
+        </div>
       </nav>
     );
   }
